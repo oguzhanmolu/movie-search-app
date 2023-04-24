7ea30413b3afb59ea71e291e4f2f4e0d1c6039a6
oguzhanmolu <oguzhanmolu@pop-os.localdomain>
2023-04-24 20:54:56 +0300

    add favicon, assets components styles folder, add globalstyles and header main footer FCs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import styled from 'styled-components';
+// Components
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <AppWrapper>
+      <Header />
+      <Main />
+      <Footer />
+    </AppWrapper>
+  );
 };
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+`;
 
 export default App;
